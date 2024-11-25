538b63429caf07cd16e5957975768dacc0ad46f3
zhang ruichu <zhangruichu@cyjiaomu.com>
2024-11-25 09:24:29 +0800

    第一次提交

---
+++ pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="content" >
+		<tab></tab>
+		<firstNav></firstNav>
+		<!-- <videoList :list="list"></videoList> -->
+	</view>
+</template>
+
+<script>
+	import tab from '../../components/tab.vue'
+	import firstNav from '../../components/first-nav.vue'
+	import videoList from '../../components/videoList.vue'
+	
+	export default {
+		data() {
+			return {
+				list:[]
+			}
+		},
+		components:{
+			tab,
+			firstNav,
+			videoList,
+		},
+		// onLoad() {
+		// 	this.getData()
+		// },
+		methods: {
+			getData(){
+				uni.request({
+					url:"http://192.168.43.101:80/api/va.json",
+					header:{
+						
+					},
+					success:(res)=> {
+						this.list=res.data.list
+					}
+					
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content{
+		width: 100%;
+		height: 100vh;
+		background: #000000;
+	}
+
+</style>
